4b33786247bb87dafafa685091d5fe7c83952461
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-01-03 20:13:00 +0300

    Add PP-DSL script to create a file in /tmp
    For task 0 of Solo-Project: - 0x0A-configuration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create `school` file inside /tmp
+
+file { 'school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
