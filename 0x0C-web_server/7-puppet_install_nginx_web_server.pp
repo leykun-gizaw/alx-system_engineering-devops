610407e4233bf78b13561814f43aa7f9e8376239
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-01-10 20:03:15 +0300

    Remove mis-spelled file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,58 +1,65 @@
 # Puppet manifest to perform all previous tasks
-exec { 'update apt repository':
-  command => 'sudo apt update',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+# 1. send apt update to puppet agent
+# 2. make sure nginx is installed
+# 3. make sure home page file is present
+# 4. make sure custom 404 page is setup
+# 5. update nginx config.
+# 6. restart nginx server
+exec { 'Update the apt repository':
+  command => 'apt update',
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
 
-package { 'nginx':
-  ensure   => present,
-  provider => apt,
+package { 'The web server':
+  ensure          => installed,
+  name            => 'nginx',
+  provider        => 'apt',
+  install_options => ['-y']
 }
 
-file { 'home page':
+file { 'The home page':
   ensure  => file,
   path    => '/var/www/html/index.html',
-  mode    => '+rw',
-  content => 'Hello, World!\n'
+  mode    => '0744',
+  owner   => 'www-data',
+  content => "Hello World!\n"
 }
 
-file { '404 page':
+file { 'The 404 page':
   ensure  => file,
   path    => '/var/www/error/custom_404.html',
-  mode    => '+rw',
-  content => 'Ceci n\'est pas une page\n'
+  mode    => '0744',
+  owner   => 'www-data',
+  content => "Ceci n'est pas une page\n"
 }
 
-file { 'default nginx config file':
+file { 'Nginx server config file':
   ensure  => file,
-  path    => '/etc/nginx/sites-available/default',
-  mode    => '+rw',
-  content => '\
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-
-    error_page 404 /custom_404.html;
-    location /custom_404.html {
-        root /var/www/error;
-        internal;
-    }
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location /redirect_me/ {
-        rewrite ^(.*)$ http://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-    }
-}'
+  path    => '/etc/nginx/sites-enabled/default',
+  mode    => '0744',
+  owner   => 'www-data',
+  content =>
+"server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+	server_name _;
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+	if (\$request_filename ~ redirect_me){
+		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+	}
+	error_page 404 /404.html;
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
+}"
 }
 
-exec { 'restart nginx':
+exec { 'Start the server':
   command => 'service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
