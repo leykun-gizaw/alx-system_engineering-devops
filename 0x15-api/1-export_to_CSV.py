a6b8dd9697e56a06733a588468f52265610d5b4a
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-01-31 17:29:05 +0300

    PY script to export task status of users to csv.
    For task 1 of Solo-Project: - 0x15-api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -26,7 +26,7 @@ class Fetch:
         return None
 
     def project_task_one(self):
-        """Returns information about employee TODO list progress
+        """Return information about employee TODO list progress
 
         Args:
             self (object): <class 'main.Fetch'> type object
@@ -34,19 +34,41 @@ class Fetch:
         Returns:
             Comperhensive string of employee TODO list progress
         """
-        brief, completed = "", ""
-        try:
-            brief = "Employee {:s} is done with tasks ({:d}/{:d}):\n".format(
-                self.name,
-                len(Fetch.__tasks(self.todos)["complete"]),
-                len(self.todos)
-            )
-            completed = Fetch.__completed_tasks(
-                Fetch.__tasks(self.todos)["complete"]
-            )
-        except Exception:
-            pass
+        brief = "Employee {:s} is done with tasks ({:d}/{:d}):\n".format(
+            self.userName,
+            len(Fetch.__tasks(self.todos)["complete"]),
+            len(self.todos)
+        )
+        completed = Fetch.__completed_tasks(
+            Fetch.__tasks(self.todos)["complete"]
+        )
         return [brief, completed]
+
+    def project_task_two(self):
+        """Export data in CSV format.
+
+        Records all tasks that are owned by this employee
+        Format: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+
+        Args:
+            self (object): <class 'main.Fetch'> type object
+
+        Returns:
+            None
+        """
+        todos = deepcopy(self.todos)
+        with open("{:s}.csv".format(self.userId), 'w') as file:
+            fieldnames = ["userId", "name", "completed", "title"]
+            writer = csv.DictWriter(
+                file,
+                quoting=csv.QUOTE_ALL,
+                fieldnames=fieldnames
+            )
+            for obj in todos:
+                obj.pop("id", None)
+                obj["name"] = self.userName
+                writer.writerow(obj)
+        return None
 
     @staticmethod
     def __tasks(tasks_list):
@@ -90,9 +112,10 @@ class Fetch:
 
 
 if __name__ == "__main__":
+    from copy import deepcopy
+    import csv
     import requests
     import sys
 
     fetch = Fetch(sys.argv[1])
-    for elem in fetch.project_task_one():
-        print(elem, end="")
+    fetch.project_task_two()
