4b841d2fcd7abf861899a1ca130b1c1f7d728606
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-03-29 10:02:43 +0300

    change os config to allow login with user 'holberton'

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+#Allow for multiple concurrent requests on nginx
 exec { 'fix--for-nginx':
   command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
 /etc/default/nginx; service nginx restart\"",
